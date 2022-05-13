be261fc09646a9f87c72a1919e9bf7a9c0324248
Oghenero Ologe <59165938+Nero2005@users.noreply.github.com>
2022-05-13 14:46:43 +0100

    Added phone number verification

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,5 +1,5 @@
 const userCtrl = require("../controllers/userCtrl");
-const authToken = require("./auth");
+const { authToken, authOTPVerified } = require("./auth");
 
 const userRouter = require("express").Router();
 
@@ -7,10 +7,16 @@ userRouter.route("/api/v1/users").get(authToken, (req, res) => {
   res.send("User test is successful");
 });
 
-userRouter.route("/api/v1/users/register").post(userCtrl.register);
+// userRouter.route("/api/v1/users/register").post(userCtrl.register);
+userRouter
+  .route("/api/v1/users/set_password")
+  .post(authOTPVerified, userCtrl.setPassword);
+userRouter
+  .route("/api/v1/users/add_personal_info")
+  .post(authOTPVerified, userCtrl.addPersonalInfo);
 userRouter.route("/api/v1/users/login").post(userCtrl.login);
 userRouter.route("/api/v1/users/logout").post(userCtrl.logout);
 
-userRouter.route("/api/v1/users/getUser").get(authToken, userCtrl.getUser);
+userRouter.route("/api/v1/users/get_user").get(authToken, userCtrl.getUser);
 
 module.exports = userRouter;
